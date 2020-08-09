a678c9dbdddee5a1211480bed156e98548f3e528
bbedevian <brettbedevian@gmail.com>
2020-08-09 12:26:03 -0400

    basic activity feed with hard data

--- src/containers/ActivityFeed.js
+++ src/containers/ActivityFeed.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import {connect} from 'react-redux'
+import ActivityCard from '../components/ActivityCard';
 
-const ActivityFeed = () => {
+const ActivityFeed = (props) => {
+    const {connections, activities} = props
+    let number = activities.length
+    let connectIDs = connections.map(connection => connection.id)
+    let completed = activities.filter(activity => activity.status === 'completed')
+    // let connectionActivities = needs to filter eventual list of all activities to only those from your connections
+    // this may also happen earlier in app
+    console.log('connectIDs :>> ', connectIDs);
     return (
         <div>
-            Activity Feed
+            <h5>Activity Feed</h5>
+            <p>there are currently {number} activities in the db total</p>
+            <p>completed activities</p>
+            {completed.map(activity => <ActivityCard key={activity.id} activity={activity}/>)}
         </div>
     );
 }
+const msp = (state) => {
+    return {
+        connections: state.connections,
+        activities: state.activities
+    }
+}
 
-export default ActivityFeed;
+export default connect(msp)(ActivityFeed);
