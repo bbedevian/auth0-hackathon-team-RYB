1bee3a4f36c6cf8f557b57fc8d9c2d5dc35256dd
MisterRK <robert.keller22@gmail.com>
2020-08-08 21:46:42 -0400

    Got a basic log in and log out with Auth0. Need to customize based on values we are looking for

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import FnFList from './containers/FnFList';
+import {Auth0Provider, useAuth0} from "@auth0/auth0-react"
+import LoginButton from './components/LoginButton'
+import LogoutButton from './components/LogoutButton'
+import Profile from './components/Profile'
 
 function App() {
+  const {user, isAuthenticated} = useAuth0()
+  console.log(isAuthenticated)
+  console.log(user)
   return (
-    <div className="App">
-     <FnFList/>
-    </div>
+    <Auth0Provider
+    domain="dev-9-s-7qkj.us.auth0.com"
+    clientId="s1xEinrrqh4VLm1Vf9Ji1F1ciz4Rm4oA"
+    redirectUri="http://localhost:3000">
+      <div className="App">
+        <FnFList/>
+      </div>
+      <LoginButton/>
+      <LogoutButton/>
+      <div>Profile Component Below</div>
+      <Profile/>
+    </Auth0Provider>
   );
 }
 
--- src/containers/FnFList.js
+++ src/containers/FnFList.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import {connect} from 'react-redux'
-import FnFCard from '../components/FnFCard';
+import React from "react";
+import { connect } from "react-redux";
+import FnFCard from "../components/FnFCard";
 
 const FnFList = (props) => {
-    return (
-        <div>
-            <h1>Family</h1>
-           { props.connections.filter(connect => connect.relationship === 'family').map(
-                connection => <FnFCard person={connection}/>
-            )}
-            <h1>Friends</h1>
-           { props.connections.filter(connect => connect.relationship === 'friend').map(
-                connection => <FnFCard person={connection}/>
-            )}
-        </div>
-    );
-}
+	return (
+		<div>
+			<h1>Family</h1>
+			{props.connections
+				.filter((connect) => connect.relationship === "family")
+				.map((connection) => (
+					<FnFCard person={connection} />
+				))}
+			<h1>Friends</h1>
+			{props.connections
+				.filter((connect) => connect.relationship === "friend")
+				.map((connection) => (
+					<FnFCard person={connection} />
+				))}
+		</div>
+	);
+};
 
 const msp = (state) => {
-    return {
-        connections: state.connections
-    }
-}
+	return {
+		connections: state.connections,
+	};
+};
 
 export default connect(msp)(FnFList);
